a258b7e5024e387b03df3d30f06407e0c36921cd
m2m2dou <mamadouabass.diallo@der.sn>
2021-08-03 12:07:29 +0000

    add scheduled function

---
+++ src/main/java/sn/esp/amigoscodeapi/cron/CronScheduled.java
@@ -0,0 +1,14 @@
+package sn.esp.amigoscodeapi.cron;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/*@Component
+@Slf4j*/
+public class CronScheduled {
+  /*  @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedRateTask() {
+        log.info("Fixed rate task - {} ", System.currentTimeMillis() / 1000);
+    }*/
+}
